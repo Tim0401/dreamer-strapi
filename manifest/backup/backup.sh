3498c88039bf48469b539bcf0043e8410fd95c17
Tsukasa Murata <stepfootprint@gmail.com>
2020-08-26 23:31:25 +0900

    backupスクリプト修正

--- manifest/backup/backup.sh
+++ manifest/backup/backup.sh
@@ -4,9 +4,9 @@ mkdir -p ~/backup
 mkdir -p ~/backup/mongo
 mkdir -p ~/backup/strapi
 
-cd ~/backup/mongo
 NOW=`date '+%Y%m%d%H%M%S'`
-zip -r ${NOW}.zip /tmp/hostpath-provisioner/mongo-backup-pvc
 
-cd ~/backup/strapi
-zip -r ${NOW}.zip /tmp/hostpath-provisioner/strapi-pvc
+cd /tmp/hostpath-provisioner
+
+zip -r ~/backup/mongo/${NOW}.zip mongo-backup-pvc
+zip -r ~/backup/strapi/${NOW}.zip strapi-pvc
